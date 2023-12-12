ffc37ec10630366779272ea2ec0a0802670d1966
viclab.kaist@gmail.com <viclab.kaist@gmail.com>
2023-12-12 17:54:49 +0900

    upload datasets dir

---
+++ datasets/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
+from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
+from .cityscapes_evaldataset import CityscapesEvalDataset
+from .dgp_dataset import DGPDataset
